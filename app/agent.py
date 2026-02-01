7f4cdf7e97b014de7343a99be4c821f41713ded2
gtrottier <gtrottier16@gmail.com>
2026-02-01 13:09:25 -0500

    docs: removed type-checker related comment after changing checker (ty -> pyrefly)

--- app/agent.py
+++ app/agent.py
@@ -18,13 +18,14 @@ tools = [scrape_product_data, analyze_sentiment, analyze_trends, generate_report
 tool_node = ToolNode(tools)
 
 llm = ChatOpenAI(
-        base_url=os.getenv("OPENROUTER_BASE_URL", "https://openrouter.ai/api/v1"),
-        api_key=os.getenv("OPENROUTER_API_KEY", ""),
-        model="google/gemini-2.5-flash",
-        temperature=0,
-    )
+    base_url=os.getenv("OPENROUTER_BASE_URL", "https://openrouter.ai/api/v1"),
+    api_key=os.getenv("OPENROUTER_API_KEY", ""),
+    model="google/gemini-2.5-flash",
+    temperature=0,
+)
 
 llm_with_tools = llm.bind_tools(tools)
+
 
 def call_model(state: AgentState):
     """Invoke le llm avec 'state' actuel"""
@@ -32,18 +33,18 @@ def call_model(state: AgentState):
     response = llm_with_tools.invoke(messages)
     return {"messages": [response]}
 
+
 def should_continue(state: AgentState) -> Literal["tools", "__end__"]:
     """Détermine si le processus doit continuer basé sur le dernier message"""
     messages = state["messages"]
     last_message = messages[-1]
-    
+
     if isinstance(last_message, AIMessage) and last_message.tool_calls:
         return "tools"
     return "__end__"
-    
-#Note : typechecker 'ty' renvoi erreur, mais fonctionne correctement. À suivre
-workflow = StateGraph(AgentState) # ty: ignore[invalid-argument-type]
 
+
+workflow = StateGraph(AgentState)
 workflow.add_node("agent", call_model)
 workflow.add_node("tools", tool_node)
 
